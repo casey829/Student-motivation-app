3cd3cfd940f5d5656d4ce2187ee46633089f263f
TITAN <aluochmarion7@gmail.com>
2024-08-05 15:28:33 +0300

    First Commit

---
+++ src/FrontEnd/StaffDashboard.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+function StaffDashboard(){
+    return (
+        <div className="dashboard">
+            <div className="logo"></div>
+            <div className="title">Dashboard</div>  
+        </div>
+        
+    )
+}
+
+
+
+
+export default StaffDashboard;
