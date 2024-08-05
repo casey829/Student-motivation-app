bac2c8f368de615ad0f41415143135c93c605240
TITAN <aluochmarion7@gmail.com>
2024-08-05 17:02:27 +0300

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import react from 'react';import AdminDashboard from './FrontEnd/Components/AdminDashboard';
+import react from 'react';
 import './App.css'
-
+import StaffDashboard from './FrontEnd/Components/StaffDashboard';
+import AdminDashboard from './FrontEnd/Components/AdminDashboard';
 function App() {
   return (
    <div>
-  
+  <StaffDashboard/>
+  <AdminDashboard/>
     </div>
   )
 }
--- src/FrontEnd/Components/AdminDashboard.jsx
+++ src/FrontEnd/Components/AdminDashboard.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import { CiFlag1 } from "react-icons/ci";
+import { FcApprove } from "react-icons/fc";
+import { IoIosArrowDropdown } from "react-icons/io";
+import { IoAddCircleOutline } from "react-icons/io5";
+import { CiVideoOn } from "react-icons/ci";
 
 
 function AdminDashboard(){
@@ -6,6 +11,8 @@ return (
     <div className="dashboard">
     <div className="header">
       <div className="header-right">
+      <div className="add-icon"><IoAddCircleOutline /></div>
+   <IoAddCircleOutline />
         <p className="user-name">Casey</p>
         <i className="logout-icon"></i>
       </div>
@@ -23,32 +30,34 @@ return (
           <button>Student</button>
         </div>
       </div>
-      <div className="mains-content">
-        <div className="video-section">
-          <i className="video-icon"></i>
-          <div className="actions">
-            <button>Categorize</button>
-            <button>Approve</button>
-            <button>Flag</button>
+      
+      <div className="content">
+          <div className="video-section">
+            <h2>Categorize Uploaded Videos</h2>
+            <div className="actions">
+              <div><CiVideoOn /></div>
+            <button>Categorize <IoIosArrowDropdown /></button>
+            <button>Approve <FcApprove /></button>
+            <button>Flag <CiFlag1 /></button>
+          </div>
+          </div>
+          <div className="audio-section">
+            <h2>Categorize Uploaded Audios</h2>
+            <div className="actions">
+            <button>Categorize <IoIosArrowDropdown /></button>
+            <button>Approve <FcApprove /></button>
+            <button>Flag <CiFlag1 /></button>
+          </div>
+          </div>
+          <div className="article-section">
+            <h2>Categorize Uploaded Articles</h2>
+            <div className="actions">
+            <button>Categorize <IoIosArrowDropdown /></button>
+            <button>Approve <FcApprove /></button>
+            <button>Flag <CiFlag1 /></button>
+          </div>
           </div>
         </div>
-        <div className="audio-section">
-          <i className="audio-icon"></i>
-          <div className="actions">
-            <button>Categorize</button>
-            <button>Approve</button>
-            <button>Flag</button>
-          </div>
-        </div>
-        <div className="article-section">
-          <i className="article-icon"></i>
-          <div className="actions">
-            <button>Categorize</button>
-            <button>Approve</button>
-            <button>Flag</button>
-          </div>
-        </div>
-      </div>
     </div>
   </div>
 ); 
