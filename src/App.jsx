af93b7296277a6f9f70b541ed8520e87439aec74
casey <caseymwangi31@gmail.com>
2024-08-09 01:13:07 +0300

    updating the landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 
 import React from "react";
 import './App.css'
+import StudentMotivation from "./FrontEnd/Components/Homepage/StudentMotivation";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <StudentMotivation />
   );
 }
 
