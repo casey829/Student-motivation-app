82cced5a88313c51dda121342dcadb55bf596d36
casey <caseymwangi31@gmail.com>
2024-08-09 14:55:20 +0300

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,15 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import React from "react";
 import './App.css'
 import StudentMotivation from "./FrontEnd/Components/Homepage/StudentMotivation";
+import StudentDashboard from './FrontEnd/Components/Dashboard/StudentDashboard';
 
 function App() {
   return (
+    <>
     <StudentMotivation />
+    <StudentDashboard />
+    
+   </>
   );
 }
 
